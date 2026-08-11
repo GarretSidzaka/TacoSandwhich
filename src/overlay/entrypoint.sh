05306fd0a519d11627ada791767adecee050a8cd
GarretSidzaka <contact@2enp.com>
2026-08-10 22:18:35 -0700

    Add entrypoint script for xrdp and supervisord

---
+++ src/overlay/entrypoint.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Remove existing sesman/xrdp PID files to prevent rdp sessions hanging on container restart
+[ ! -f /var/run/xrdp/xrdp-sesman.pid ] || rm -f /var/run/xrdp/xrdp-sesman.pid
+[ ! -f /var/run/xrdp/xrdp.pid ] || rm -f /var/run/xrdp/xrdp.pid
+
+# Start xrdp sesman service
+/usr/sbin/xrdp-sesman
+
+# Start supervisord
+echo
+print_header "Starting supervisord";
+print_step_header "Logging all root services to '/var/log/supervisor/'"
+print_step_header "Logging all user services to '/home/${USER:?}/.cache/log/'"
+echo
+mkdir -p /var/log/supervisor
+chmod a+rw /var/log/supervisor
+exec /usr/bin/supervisord -c /etc/supervisord.conf --nodaemon --user root
+
+
+# Run xrdp in foreground if no commands specified
+if [ -z "$1" ]; then
+    /usr/sbin/xrdp --nodaemon
+else
+    /usr/sbin/xrdp
+    exec "$@"
+fi
+
